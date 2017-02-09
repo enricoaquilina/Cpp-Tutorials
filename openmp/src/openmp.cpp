ddb7c24bacdc7c0862192f9eefc6ec2a9c9dcefa
enricoaquilina <tappiera00@gmail.com>
2017-02-09 11:26:14 +0100

    parallelizing PI program

--- openmp/src/openmp.cpp
+++ openmp/src/openmp.cpp
@@ -12,11 +12,33 @@
 using namespace std;
 
 int main() {
+	double A[1000];
+	//create a 4 thread parallel region in the next parallel region
+	//i.e. pragma omp parallel
 
-	#pragma omp parallel
+//	omp_set_num_threads(4);
+	long num_steps = 10000000;
+	double step;
+	int i;
+	double x, pi, sum, aux = 0.0;
+
+	step = 1.0/(double)num_steps;
+	#pragma omp parallel num_threads(8) private(i,x,aux) shared(sum)
 	{
-		int ID = omp_get_thread_num();
-		printf(" hello(%d)", ID);
-		printf(" world(%d)\n", ID);
+	#pragma omp for schedule(static)
+//		int cur_id = omp_get_thread_num();
+//		printf("\ncur id(%d)",cur_id);
+//		printf("\ni->(%d)",i);
+
+		for(i=0;i<num_steps;i++){
+			x=(i+0.5)*step;
+			aux = 4.0/(1.0+x*x);
+
+	#pragma omp critical
+			sum += aux;
+			printf("\nsum is %f", sum);
+		}
 	}
+	pi=step*sum;
+	cout<<"\nValue of PI is "<<pi<<endl;
 }
